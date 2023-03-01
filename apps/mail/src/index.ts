181299f9955340cc03309561dc9eb511eaae9e0c
Victor Santele <victor.santele@gmail.com>
2023-03-01 19:45:58 +0100

    fix: fetch new mails

--- apps/mail/src/index.ts
+++ apps/mail/src/index.ts
@@ -11,48 +11,44 @@ function openInbox(cb: (err: Error, box: Box) => void) {
 
 imap.once("ready", () => {
 	logger.info("Ready");
-	openInbox(function (err, box) {
-		if (err) throw err;
-	});
+	openInbox(function (err, box) {});
 });
 
 imap.on("mail", (nbNewMail: number) => {
 	openInbox(function (err, box) {
-		if (err) throw err;
-		imap.search(["NEW"], function (err, results) {
-			if (err || !results.length) return;
+		if (err) {
+			logger.error(`Open inbox: ${err}`);
+			return;
+		}
 
-			// fetch all resulting messages
-			const f = imap.fetch(results, {
-				// bodies: ["HEADER.FIELDS (FROM TO SUBJECT DATE)", "TEXT"],
-				bodies: "",
-				struct: true,
-				markSeen: true,
-			});
+		const f = imap.fetch(`${box.messages.total}:*`, {
+			bodies: "",
+			struct: true,
+			markSeen: true,
+		});
 
-			f.on("message", function (msg, seqno) {
-				logger.info("Message #%d", seqno);
-				var prefix = `(#${seqno}) `;
-				msg.on("body", function (stream, info) {
-					var buffer = "";
-					stream.on("data", function (chunk) {
-						buffer += chunk.toString("utf8");
-					});
-					stream.once("end", async function () {
-						let parsed = await simpleParser(buffer);
-						processMail(parsed);
-					});
+		f.on("message", function (msg, seqno) {
+			logger.info("Message #%d", seqno);
+			var prefix = `(#${seqno}) `;
+			msg.on("body", function (stream, info) {
+				var buffer = "";
+				stream.on("data", function (chunk) {
+					buffer += chunk.toString("utf8");
 				});
-				msg.once("end", function () {
-					logger.info(`${prefix}Finished`);
+				stream.once("end", async function () {
+					let parsed = await simpleParser(buffer);
+					processMail(parsed);
 				});
 			});
-			f.once("error", function (err) {
-				logger.error(`Fetch error: ${err}`);
+			msg.once("end", function () {
+				logger.info(`${prefix}Finished`);
 			});
-			f.once("end", function () {
-				logger.info("Done fetching all messages!");
-			});
+		});
+		f.once("error", function (err) {
+			logger.error(`Fetch error: ${err}`);
+		});
+		f.once("end", function () {
+			logger.info("Done fetching all messages!");
 		});
 	});
 });
