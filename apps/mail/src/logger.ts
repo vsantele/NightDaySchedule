44eaf57b2520b6e7d5f3e5d8af5f0a71f3d5e764
Victor Santele <victor.santele@gmail.com>
2023-03-01 10:55:34 +0100

    fix: log

--- apps/mail/src/logger.ts
+++ apps/mail/src/logger.ts
@@ -2,17 +2,23 @@ import pino from "pino";
 
 const isProd = process.env.NODE_ENV === "production";
 
-const filePipeline = {
-	level: "info",
-	target: "pino/file",
-	options: { destination: "/var/logs/mail/mail.log" },
-};
-const consolePipeline = {
-	target: isProd ? "pino/file" : "pino-pretty",
-};
-
 const transport = pino.transport({
-	pipeline: isProd ? [filePipeline, consolePipeline] : [consolePipeline],
+	targets: [
+		{
+			level: "info",
+			target: "pino/file",
+			options: {
+				destination: isProd
+					? "/var/logs/mail/mail.log"
+					: `${process.env.TMP}night.log`,
+			},
+		},
+		{
+			level: isProd ? "info" : "debug",
+			target: isProd ? "pino/file" : "pino-pretty",
+			options: { destination: undefined },
+		},
+	],
 });
 
 const logger = pino(
